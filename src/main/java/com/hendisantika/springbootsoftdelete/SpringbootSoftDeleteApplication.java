1e56cdf15cb6cf14ed0a795456b0339e0ba282f0
Hendi Santika <hendisantika@gmail.com>
2021-06-13 18:01:35 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsoftdelete/SpringbootSoftDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsoftdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoftDeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSoftDeleteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootsoftdelete/SpringbootSoftDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsoftdelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSoftDeleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
