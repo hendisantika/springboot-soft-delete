802820a9a04d00e4712951747cac02b86ba39ebb
Hendi Santika <hendisantika@gmail.com>
2021-06-13 18:08:47 +0700

    #6 - Add Book Service

---
+++ src/main/java/com/hendisantika/springbootsoftdelete/service/BookService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootsoftdelete.service;
+
+import com.hendisantika.springbootsoftdelete.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-soft-delete
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/06/21
+ * Time: 18.08
+ */
+@Service
+public class BookService {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private BookRepository bookRepository;
+}
