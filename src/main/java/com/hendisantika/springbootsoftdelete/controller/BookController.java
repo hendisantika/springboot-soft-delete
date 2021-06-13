6c75b6fb60db505fe826bb3f82a3cfb61e4a7def
Hendi Santika <hendisantika@gmail.com>
2021-06-13 18:13:59 +0700

    #13 - Add findAll in BookController

--- src/main/java/com/hendisantika/springbootsoftdelete/controller/BookController.java
+++ src/main/java/com/hendisantika/springbootsoftdelete/controller/BookController.java
@@ -4,10 +4,12 @@ import com.hendisantika.springbootsoftdelete.entity.Book;
 import com.hendisantika.springbootsoftdelete.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,5 +36,10 @@ public class BookController {
     @DeleteMapping("/{id}")
     public void removeOne(@PathVariable("id") Long id) {
         bookService.remove(id);
+    }
+
+    @GetMapping
+    public Iterable<Books> findAll(@RequestParam(value = "isDeleted", required = false, defaultValue = "false") boolean isDeleted) {
+        return bookService.findAll(isDeleted);
     }
 }
