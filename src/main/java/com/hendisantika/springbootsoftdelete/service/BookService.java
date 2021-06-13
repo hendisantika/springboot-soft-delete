846508fbf9ed8e5ecb638681b070c8e0212206b8
Hendi Santika <hendisantika@gmail.com>
2021-06-13 18:09:30 +0700

    #7 - Add create in Book Service

--- src/main/java/com/hendisantika/springbootsoftdelete/service/BookService.java
+++ src/main/java/com/hendisantika/springbootsoftdelete/service/BookService.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springbootsoftdelete.service;
 
+import com.hendisantika.springbootsoftdelete.entity.Book;
 import com.hendisantika.springbootsoftdelete.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,4 +24,8 @@ public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    public Book create(Book books) {
+        return bookRepository.save(books);
+    }
 }
